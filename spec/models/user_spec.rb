f4cde01aebf9d2fd9274a52d615440dc8f84605a
emiko <31835314+siroemk@users.noreply.github.com>
2023-07-07 13:14:36 +0900

    テストが通るように修正した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe User, type: :model do
   it 'is invalid without a name' do
     user = User.new(name: nil)
     user.valid?
-    expect(user.errors[:name]).to_not include("can't be blank")
+    expect(user.errors[:name]).to include("can't be blank")
   end
 end
