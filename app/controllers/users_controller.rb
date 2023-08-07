ea87b5d1f479175376803616df890f09a8a04d65
emiko <31835314+siroemk@users.noreply.github.com>
2023-08-07 12:23:29 +0900

    アカウント削除ボタンを作成した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@
 
 class UsersController < ApplicationController
   def edit; end
+
+  def destroy
+    current_user.destroy!
+    reset_session
+    redirect_to root_path, notice: 'アカウントを削除しました'
+  end
 end
