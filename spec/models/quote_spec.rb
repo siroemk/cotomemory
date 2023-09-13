e118621ddaa11b143b13f51b5b4de4bc05cbfd47
emiko <31835314+siroemk@users.noreply.github.com>
2023-09-13 11:21:18 +0900

    名言が空の場合、登録が失敗することを確認するテストを追加した

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe 'Quote', type: :model do
     quote = build(:quote)
     expect(quote).to be_valid
   end
+
+  context '名言が空の場合' do
+    it '登録失敗すること' do
+      quote = build(:quote, content: nil)
+      quote.valid?
+      expect(quote.errors[:content]).to include('を入力してください')
+    end
+  end
 end
