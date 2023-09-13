b2ab5994ca5131dd93c784759627ced35f0766ee
emiko <31835314+siroemk@users.noreply.github.com>
2023-09-13 13:19:53 +0900

    invitation_tokenが空だと登録できないテストを追加した

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe 'Family', type: :model do
     family = build(:family)
     expect(family).to be_valid
   end
+
+  context 'invitation_tokenが空の場合' do
+    it '登録失敗すること' do
+      family = build(:family, invitation_token: nil)
+      family.valid?
+      expect(family.errors[:invitation_token]).to include('を入力してください')
+    end
+  end
 end
