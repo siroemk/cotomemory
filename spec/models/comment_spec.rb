45c99c447eb73389bcc9db4015d639183ee5b857
emiko <31835314+siroemk@users.noreply.github.com>
2023-09-13 13:37:52 +0900

    quoteやuserが空の場合のテストを追加した

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe 'Comment', type: :model do
       expect(comment.errors[:content]).to include('を入力してください')
     end
   end
+
+  context 'quoteが空の場合' do
+    it '登録失敗すること' do
+      comment = build(:comment, quote: nil)
+      comment.valid?
+      expect(comment.errors[:quote]).to include('を入力してください')
+    end
+  end
+
+  context 'userが空の場合' do
+    it '登録失敗すること' do
+      comment = build(:comment, user: nil)
+      comment.valid?
+      expect(comment.errors[:user]).to include('を入力してください')
+    end
+  end
 end
