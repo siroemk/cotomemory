d565a918374286c6b4c2978b8ff1da77dcd043fa
emiko <31835314+siroemk@users.noreply.github.com>
2023-09-13 11:59:10 +0900

    こどもの名前が空だと登録できないテストを追加した

--- spec/models/child_spec.rb
+++ spec/models/child_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe 'Child', type: :model do
     child = build(:child)
     expect(child).to be_valid
   end
+
+  context 'こどもの名前が空の場合' do
+    it '登録失敗すること' do
+      child = build(:child, name: nil)
+      child.valid?
+      expect(child.errors[:name]).to include('を入力してください')
+    end
+  end
 end
