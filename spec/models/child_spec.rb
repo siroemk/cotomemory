af4ad8321966d94e03bacbfb64906e444144dbcc
emiko <31835314+siroemk@users.noreply.github.com>
2023-09-13 12:05:07 +0900

    こどもの誕生日が空だと登録できないテストを追加した

--- spec/models/child_spec.rb
+++ spec/models/child_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe 'Child', type: :model do
       expect(child.errors[:name]).to include('を入力してください')
     end
   end
+
+  context 'こどもの誕生日が空の場合' do
+    it '登録失敗すること' do
+      child = build(:child, date_of_birth: nil)
+      child.valid?
+      expect(child.errors[:date_of_birth]).to include('を入力してください')
+    end
+  end
 end
