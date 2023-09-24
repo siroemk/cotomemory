34753618c79ed5d4b61da3f4cb8396b166c90fa8
emiko <31835314+siroemk@users.noreply.github.com>
2023-09-24 12:47:44 +0900

    誕生の年と月を計算するメソッドを追加

---
+++ app/helpers/quotes_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module QuotesHelper
+  def year_of_birth(date_of_birth)
+    passed_month = (Time.zone.now - date_of_birth.to_time).to_i / 1.month
+    (passed_month % 12).to_s
+  end
+
+  def month_of_birth(date_of_birth)
+    passed_month = (Time.zone.now - date_of_birth.to_time).to_i / 1.month
+    (passed_month / 12).to_s
+  end
+end
