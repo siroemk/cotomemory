2217282240315e3a8ab01744fc4a7493cb980a5c
emiko <31835314+siroemk@users.noreply.github.com>
2023-10-17 12:08:19 +0900

    誕生日の年と月の計算を修正

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -3,11 +3,11 @@
 module QuotesHelper
   def year_of_birth(date_of_birth)
     passed_month = (Time.zone.now - date_of_birth.to_time).to_i / 1.month
-    (passed_month % 12).to_s
+    (passed_month / 12).to_s
   end
 
   def month_of_birth(date_of_birth)
     passed_month = (Time.zone.now - date_of_birth.to_time).to_i / 1.month
-    (passed_month / 12).to_s
+    (passed_month % 12).to_s
   end
 end
