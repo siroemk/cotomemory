39d2045595b19971d15d572d7767b80768010cd5
emiko <31835314+siroemk@users.noreply.github.com>
2023-10-18 13:15:00 +0900

    テストの追加

--- spec/system/quotes_spec.rb
+++ spec/system/quotes_spec.rb
@@ -4,16 +4,44 @@ require 'rails_helper'
 
 RSpec.describe 'Quotes', type: :system do
   let(:user) { create(:user) }
+  let(:my_child) { create(:child, family: user.family) }
+  before do
+    sign_in_as(user)
+  end
 
   it '名言を登録できること' do
-    sign_in_as(user)
-    fill_in 'child[name]', with: '花子'
-    click_on '保存する'
-
+    create(:child, family: user.family)
+    visit quotes_path
     click_on '登録する'
     fill_in 'quote[content]', with: 'テストの名言です'
-    click_on '登録する'
+    click_on '保存する'
+    expect(page).to have_content '名言を登録しました'
+
     expect(current_path).to eq quotes_path
     expect(page).to have_content 'テストの名言です'
+  end
+
+  it '名言を編集できること' do
+    create(:quote, child: my_child, user:)
+    visit quotes_path
+    click_on '編集'
+    fill_in 'quote[content]', with: '今日もがんばりたい'
+    click_on '保存する'
+    expect(page).to have_content '名言を更新しました'
+
+    expect(current_path).to eq quotes_path
+    expect(page).to have_content '今日もがんばりたい'
+  end
+
+  it '名言を削除できること' do
+    create(:quote, child: my_child, user:)
+    visit quotes_path
+    expect(page).to have_content 'ママもお勉強がんばってね！'
+
+    click_on '削除'
+    expect(page.accept_confirm).to eq('本当に削除しますか？')
+    expect(page).to have_content '名言を削除しました'
+
+    expect(page).not_to have_content 'ママもお勉強がんばってね！'
   end
 end
