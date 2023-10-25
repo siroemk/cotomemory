ae425b4d2ab0e7008adb6f6d7ea3f6c10a25e41d
emiko <31835314+siroemk@users.noreply.github.com>
2023-10-25 16:34:55 +0900

    家族の一覧をdescからascに変更

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -3,6 +3,6 @@
 class FamiliesController < ApplicationController
   def show
     @family = Family.find(current_user.family_id)
-    @users = User.where(family_id: current_user.family_id).where.not(id: current_user.id).order(created_at: :desc)
+    @users = User.where(family_id: current_user.family_id).where.not(id: current_user.id).order(created_at: :asc)
   end
 end
