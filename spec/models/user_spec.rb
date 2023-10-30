f5171c3c2dd8c8a1e45de038e38280497a977918
emiko <31835314+siroemk@users.noreply.github.com>
2023-10-30 13:10:16 +0900

    validate_avatarのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validate_avatar' do
+    context 'when avatar is attached' do
+      let(:large_image) { fixture_file_upload(Rails.root.join('spec/fixtures/large_image.jpg'), 'image/jpg') }
+      let(:invalid_file) { fixture_file_upload(Rails.root.join('spec/fixtures/invalid_file.txt'), 'text/plain') }
+
+      it 'validates file size' do
+        user = create(:user)
+        user.avatar.attach(large_image)
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('プロフィール画像のファイルサイズは1MB以下にしてください')
+      end
+
+      it 'validates file format' do
+        user = create(:user)
+        user.avatar.attach(invalid_file)
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('プロフィール画像はjpeg,jpg,pngのいずれかにしてください')
+      end
+    end
+  end
 end
