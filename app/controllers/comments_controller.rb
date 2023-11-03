3f5d9f7ef0d5827ade18e8f2505488858a7e783c
emiko <31835314+siroemk@users.noreply.github.com>
2023-11-03 12:48:50 +0900

    リファクタリングした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_quote, only: %i[create destroy]
+
   def create
     @comment = current_user.comments.build(comment_params)
-    @comment.quote_id = params[:quote_id]
-    @quote = Quote.find(params[:quote_id])
+    @comment.quote = @quote
     if @comment.save
       respond_to do |format|
-        format.html { redirect_to quote_comments_path(:quote_id), notice: 'コメントしました' }
+        format.html { redirect_to quote_comments_path(@quote), notice: 'コメントしました' }
         format.turbo_stream { flash.now[:notice] = 'コメントしました' }
       end
     else
@@ -16,12 +17,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @quote = Quote.find(params[:quote_id])
     @comment = @quote.comments.find(params[:id])
-    if current_user.id == @comment.user.id
+    if current_user == @comment.user
       @comment.destroy
       respond_to do |format|
-        format.html { redirect_to quote_comments_path(params[:quote_id]), notice: 'コメントを削除しました' }
+        format.html { redirect_to quote_comments_path(@quote), notice: 'コメントを削除しました' }
         format.turbo_stream { flash.now[:notice] = 'コメントを削除しました' }
       end
     else
@@ -33,5 +33,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_quote
+    @quote = Quote.find(params[:quote_id])
   end
 end
