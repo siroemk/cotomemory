a25d139cf1e6ae6737e4d4193bb7319e4afaf52b
emiko <31835314+siroemk@users.noreply.github.com>
2026-05-09 10:48:47 +0900

    Fix redundant age month assignment

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -4,7 +4,7 @@ module QuotesHelper
   def convert_to_age(date_of_birth)
     age_year = (Time.zone.today.strftime('%Y%m%d').to_i - date_of_birth.strftime('%Y%m%d').to_i) / 10_000
     age_month = (Time.zone.today.strftime('%m%d').to_i - date_of_birth.strftime('%m%d').to_i) / 100
-    age_month = age_month.negative? ? age_month + 12 : age_month
+    age_month += 12 if age_month.negative?
 
     "（#{age_year}才#{age_month}ヶ月）"
   end
